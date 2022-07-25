478ea4e2ab7ebbaaff9edc57369a42341778cec2
advokate-cc <chao.huang@advokategroup.com>
2022-07-25 17:05:12 +0800

    fix: v1.0.1:	新增：	修改：	删除：

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import Layout from '@/Layouts/BasicLayout';
 
 const Dashboard = lazy(() => import('@/pages/Dashboard'));
 const Home = lazy(() => import('@/pages/Home'));
+const Canvas = lazy(() => import('@/pages/Canvas'));
 const NotFound = lazy(() => import('@/components/NotFound'));
 
 const routerConfig: IRouterConfig[] = [
@@ -12,10 +13,23 @@ const routerConfig: IRouterConfig[] = [
     children: [{
       path: '/dashboard',
       component: Dashboard,
+      pageConfig: {
+        title: '登录页面',
+      },
     }, {
       path: '/',
       exact: true,
       component: Home,
+      pageConfig: {
+        title: '首页',
+      },
+    },  {
+      path: '/demo-canvas',
+      exact: true,
+      component: Canvas,
+      pageConfig: {
+        title: 'map',
+      },
     }, {
       component: NotFound,
     }],
