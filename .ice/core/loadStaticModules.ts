5c391b33b404d04388c7fa7a5cd28a35c0dbe4a0
hd-huangchao <chao.huang@advokategroup.com>
2022-08-12 11:11:46 +0800

    base

--- .ice/core/loadStaticModules.ts
+++
@@ -1,9 +0,0 @@
-import module0 from '../plugins/request/pluginRuntime/runtime';
-
-import type { IAppConfig } from '../types';
-
-function loadStaticModules(appConfig: IAppConfig) {
-  module0({ appConfig });
-}
-
-export default loadStaticModules;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,18 @@
-import { useEffect } from 'react';
-import { useRequest } from 'ice';
-import { Table } from 'antd';
-import styles from './index.module.css';
+import Guide from '@/components/Guide';
+import { trim } from '@/utils/format';
+import { PageContainer } from '@ant-design/pro-components';
+import { useModel } from '@umijs/max';
+import styles from './index.less';
 
-export default function Home() {
-  const { data, error, loading, request: fetchRepos } = useRequest({ url: '/api/getRepos' });
-  const { dataSource = [] } = data || {};
-
-  useEffect(() => {
-    console.log('Home useEffect....');
-    (async function () {
-      await fetchRepos();
-    }());
-  }, []);
-
+const HomePage: React.FC = () => {
+  const { name } = useModel('global');
   return (
-    <div className={styles.container}>
-      <h2>Home page</h2>
-      {
-        error ? (
-          <div>request error: {error.message}</div>
-        ) : (
-          <Table
-            loading={loading}
-            dataSource={dataSource}
-            rowKey="id"
-          >
-            <Table.Column title="ID" dataIndex="id" key="id" />
-            <Table.Column title="名称" dataIndex="name" key="name" />
-            <Table.Column title="描述" dataIndex="description" key="description" />
-          </Table>
-        )
-      }
-    </div>
+    <PageContainer ghost>
+      <div className={styles.container}>
+        <Guide name={trim(name)} />
+      </div>
+    </PageContainer>
   );
-}
+};
+
+export default HomePage;
